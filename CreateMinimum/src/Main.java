97cd88e8ddc00ebdaa458e5803bb085689a0f39b
yongcowhee <yongcowhee@gmail.com>
2023-04-27 04:05:05 +0900

    - Programmers Lv.2 최솟값 만들기

---
+++ CreateMinimum/src/Main.java
@@ -0,0 +1,9 @@
+import createMinimum.*;
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int A[] = {1,4,2};
+        int B[] = {5,4,4};
+        sol.solution(A, B);
+    }
+}
