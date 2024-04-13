022c9481134811d9022f2f218717e3ec3a4a412b
heecong <yongcowhee@gmail.com>
2024-04-13 23:15:11 +0900

    [24.04.13, 성공] 생각보다 식을 생각해내기 어려웠고, 결국 검색하여 해결했다. 문제 접근방식이 잘못된 것 같아 이 부분을 고쳐나가야겠다고 다짐했다!

--- Baekjoon_2217_Rope/src/Main.java
+++ Baekjoon_2217_Rope/src/Main.java
@@ -1,34 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
-    static int[] arr;
+    static Integer[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        arr = new Integer[n];
 
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int weight = 0;
-        float tmp;
-        for (int w = 1; w < Arrays.stream(arr).sum(); w++) {
-            tmp = w/(float)n;
-            for (int i = 0; i < n; i++) {
-                if (arr[i] >= tmp){
-                    weight = w;
-                } else {
-                    break;
-                }
-            }
-        }
+        Arrays.sort(arr);
 
-        System.out.println(weight);
+        int answer = 0;
+        // 큰 중량을 버틸 수 있는 로프부터 선택하면 최대치를 구하기 더 수월해진다.
+        // 큰 중량의 로프부터 선택하면서 작은 중량의 로프를 선택했을 때, 즉 k(=i)개의 로프를 선택했을 때는 로프*k개가 최대 무게임
+        // 즉, 3, 8, 10 세 개의 로프를 사용하면 최대는 3(가장 작은) * 3(개수) = 9 일 것이다.
+        // 그리고, 8, 10을 선택하면 8 * 2 = 16 이므로, 이와 같은 방식으로 비교하면서 최대값을 산출하게 된다!
+        for (int i = 1; i <= n; i++) {
+            answer = Math.max(answer, arr[n-i] * i);
+        }
+        System.out.println(answer);
     }
 }
