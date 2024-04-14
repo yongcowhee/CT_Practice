60bd3bf5876d265088bbaad221b8ff4da09a4add
heecong <yongcowhee@gmail.com>
2024-04-14 18:02:50 +0900

    [24.04.14, 성공] Bottom-up 형식으로 DP를 이용하여 해결하였다. 익숙하지 않아서 더 공부해야 할 것 같다.

--- Baekjoon_1463_MakeOne/src/Main.java
+++ Baekjoon_1463_MakeOne/src/Main.java
@@ -14,16 +14,18 @@ public class Main {
     }
 
     private static int findMin(int x) {
-        // 1일 때는 연산 횟수가 0번이므로 0 return
-        if (x == 1) return 0;
-        // int는 원시타입이라 0으로 초기화 되는데, 0보다 크다는 것은 이미 최솟값을 찾았다는 의미로 dp[x] return
-        if (dp[x] > 0)
-            return dp[x];
-        // x가 1이 아니라는 것이 이미 입증된 상황이므로 1빼기! 이히호하히
-        dp[x] = findMin(x - 1) + 1;
-        if (x % 2 == 0) dp[x] = Math.min(dp[x], findMin(x / 2) + 1);
-        if (x % 3 == 0) dp[x] = Math.min(dp[x], findMin(x / 3) + 1);
+        dp[1] = 0;
 
+        for (int i = 2; i <= x; i++) {
+            dp[i] = dp[i - 1];
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2]);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3]);
+            }
+            dp[i] = dp[i] + 1;
+        }
         return dp[x];
     }
 }
