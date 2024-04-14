5348625692b9125142e4d9010a16c9617588113f
heecong <yongcowhee@gmail.com>
2024-04-14 16:42:06 +0900

    [24.04.14, 실패] Top-down 형식의 재귀 함수를 호출하도록 구현했는데, 시간초과가 나서 실패했다.

--- Baekjoon_1463_MakeOne/src/Main.java
+++ Baekjoon_1463_MakeOne/src/Main.java
@@ -3,20 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int count = 0;
 
-        while (n != 1) {
-            if (n % 3 != 0) {
-                n -= 1;
-                count++;
-            } else if (n % 3 == 0) {
-                n /= 3;
-                count++;
-            }
-        }
-        System.out.println(count);
+        dp = new int[n + 1];
+        System.out.println(findMin(n));
+    }
+
+    private static int findMin(int x) {
+        // 1일 때는 연산 횟수가 0번이므로 0 return
+        if (x == 1) return 0;
+        // int는 원시타입이라 0으로 초기화 되는데, 0보다 크다는 것은 이미 최솟값을 찾았다는 의미로 dp[x] return
+        if (dp[x] > 0)
+            return dp[x];
+        // x가 1이 아니라는 것이 이미 입증된 상황이므로 1빼기! 이히호하히
+        dp[x] = findMin(x - 1) + 1;
+        if (x % 2 == 0) dp[x] = Math.min(dp[x], findMin(x / 2) + 1);
+        if (x % 3 == 0) dp[x] = Math.min(dp[x], findMin(x / 3) + 1);
+
+        return dp[x];
     }
 }
