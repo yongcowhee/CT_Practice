a48bf9a7e7a6b9c175a95bfba053915397e66b3d
heecong <yongcowhee@gmail.com>
2024-05-14 23:58:00 +0900

    [24.05.14, 실패] 아무래도 dfs 같은데 잘 모르겠다. dfs 매개변수로 뭐가 들어가야 할지 아직 잘 파악이 안된다.

---
+++ Baekjoon_2579_ClimbingStairs/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    private static int n;
+    private static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+    }
+
+    private static int dfs(int depth, int )
+}
