31f0ca1637c7ee87f17c11bc4e7fa5b05c4e0255
heecong <yongcowhee@gmail.com>
2024-05-15 01:57:00 +0900

    [24.05.14, 실패 후 성공] dfs는 무슨 dp 문제였다. dfs로 풀기에는 2^n의 시간 복잡도가 나와 불가능한 문제였다. 아직 문제 유형을 파악하는 식견이 부족한 것 같아 많은 연습이 필요할 것 같다. 또한 dp로 문제를 풀이할 때, n이 자연수라는 사실을 망각하고 1, 2일 경우를 고려하지 않고 무조건 3보다 크다고 멋대로 생각하고 풀어 index 에러가 났다. 조심해야겠다.

--- Baekjoon_2579_ClimbingStairs/src/Main.java
+++ Baekjoon_2579_ClimbingStairs/src/Main.java
@@ -1,14 +1,36 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     private static int n;
     private static int[] arr;
-    public static void main(String[] args) throws IOException{
+    private static int[] dp;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
-    }
+        dp = new int[n];
 
-    private static int dfs(int depth, int )
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        if(n==1){
+            dp[0] = arr[0];
+        } else if(n==2){
+            dp[0] = arr[0];
+            dp[1] = arr[0] + arr[1];
+        } else{
+            dp[0] = arr[0];
+            dp[1] = arr[0] + arr[1];
+            dp[2] = Math.max(arr[0], arr[1]) + arr[2];
+
+            for (int i = 3; i < n; i++) {
+                dp[i] = Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i]; // 두 계단을 한 번에 오르거나, 두 계단 오르고 한 계단 오르거나
+            }
+        }
+        System.out.println(dp[n-1]);
+    }
 }
